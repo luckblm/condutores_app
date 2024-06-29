8b8494beab2a147903c635ebbad94c483710b642
luckblm <romil.roni@gmail.com>
2024-06-29 14:49:12 -0300

    Renomeando base de dados; Script para tratar os dados.

---
+++ tratamento_base_dados.R
@@ -0,0 +1,5 @@
+#Caarregar base de dados
+base_dados <- read_csv("~/Dropbox/Detran Diego Frota Licenciada/condutores_app/CONDUTOR_RENACH_SIDET.csv")
+
+#Nome da Base de dados
+base_dados %>% names()
